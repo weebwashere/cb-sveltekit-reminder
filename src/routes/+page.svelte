46378189db4ba04ae39f8d50d8e9206f687de4d6
weeb .js <ilyoreo99@gmail.com>
2023-12-18 02:54:50 +0000

    huh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,12 @@
 </script>
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
+		{#if $page.data.user}
+		<h2 class="h2">Hi {$page.data.user.name}.</h2>
+		<Avatar src={$page.data.user.avatar} />
+		{:else}
 		<h2 class="h2">You are not logged in.</h2>
+		{/if}
 	</div>
 </div>
 
