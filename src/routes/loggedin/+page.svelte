98ff6143e414d2212f9497842d1a1015766c09d7
weeb .js <ilyoreo99@gmail.com>
2023-12-18 02:38:35 +0000

    changes + new shit

---
+++ src/routes/loggedin/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+
+	import { page } from "$app/stores";
+	import { Avatar } from "@skeletonlabs/skeleton";
+
+</script>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		{#if $page.data.user}
+		<h2 class="h2">Hi {$page.data.user.name}.</h2>
+		<Avatar src={$page.data.user.avatar} />
+	</div>
+</div>
+
+<style lang="postcss">
+</style>
